9c38a6ab749fe6502fb070473df9865ae5a75e12
Olaf Penschke <35469568+olape-git@users.noreply.github.com>
2025-02-11 13:37:28 +0100

    Input fields number instead of text

--- config/metaconfig.php
+++ config/metaconfig.php
@@ -1,13 +1,18 @@
 <?php
 
+$plugin_mcf['realblog']['admin_records_page']="number";
 $plugin_mcf['realblog']['auto_archive']="bool";
 $plugin_mcf['realblog']['auto_publish']="bool";
 $plugin_mcf['realblog']['comments_plugin']="xenum:twocents";
+$plugin_mcf['realblog']['entries_per_page']="number";
 $plugin_mcf['realblog']['entries_order']="enum:desc,asc";
 $plugin_mcf['realblog']['heading_level']="enum:h1,h2,h3,h4,h5,h6";
 $plugin_mcf['realblog']['heading_above_meta']="bool";
+$plugin_mcf['realblog']['links_visible']="number";
 $plugin_mcf['realblog']['rss_enabled']="bool";
+$plugin_mcf['realblog']['rss_entries']="number";
 $plugin_mcf['realblog']['pagination_bottom']="bool";
 $plugin_mcf['realblog']['pagination_top']="bool";
+$plugin_mcf['realblog']['pagination_radius']="number";
 $plugin_mcf['realblog']['show_read_more_link']="bool";
 $plugin_mcf['realblog']['show_teaser']="bool";
